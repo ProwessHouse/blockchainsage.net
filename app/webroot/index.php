03d3207f32cacaa682617e56e5a8299dcbf5e850
Nilam Doctor <nilamdoc@gmail.com>
2019-08-03 16:18:25 +0530

    error

--- app/webroot/index.php
+++ app/webroot/index.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 if (!isset($_SERVER['HTTPS'])) {
 
 	header('Location: https://' . $_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI']);
