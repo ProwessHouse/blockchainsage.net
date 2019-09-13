c28d59d691e858d9d670679d719df2a431336bff
Nilam Doctor <nilamdoc@gmail.com>
2019-09-13 11:37:44 +0530

    today

--- app/views/register/index.html.php
+++ app/views/register/index.html.php
@@ -1,1 +1,14 @@
 <h1>Register</h1>
+<amp-img src="/app/webroot/en/images/bg_1.jpg"  layout="responsive" alt="Welcome" width="400" height="300"></amp-img>
+<style amp-custom>
+  /* any custom style goes here */
+  body {
+    background-color: gray;
+  }
+  amp-img {
+    background-color: gray;
+    border: 1px solid black;
+  }
+</style>
+
+<link rel="canonical" href="https://blockchain.local/register">
